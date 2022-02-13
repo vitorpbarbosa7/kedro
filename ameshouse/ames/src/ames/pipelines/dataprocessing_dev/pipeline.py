b39a9c7d3cf8709f85a0815c692078ad5e1b26c9
vitor <vitorpbarbosa7@gmail.com>
2022-02-13 16:08:19 -0300

    train and test sets ready

--- ameshouse/ames/src/ames/pipelines/dataprocessing_dev/pipeline.py
+++ ameshouse/ames/src/ames/pipelines/dataprocessing_dev/pipeline.py
@@ -4,7 +4,7 @@ from kedro.pipeline import Pipeline, node
 
 
 
-from .nodes import split_target, train_test_split, concat_df
+from .nodes import drop_covariates, process_categorical, split_target, train_test_split, concat_df, process_numeric, join_num_cat
 
 def create_pipeline(**kwargs):
     return Pipeline(
@@ -26,6 +26,54 @@ def create_pipeline(**kwargs):
                 ,inputs=["X_train","X_test","y_train","y_test"]
                 ,outputs=["train","test"]
                 ,name="train_test_datasets"
+            )
+            ,node(
+                func=drop_covariates
+                ,inputs=["train","parameters"]
+                ,outputs="train_dropped"
+                ,name="Drop Irrelevant Covariates - Train"
+            )
+            ,node(
+                func=process_categorical
+                ,inputs=["train_dropped","parameters"]
+                ,outputs="train_categorical"
+                ,name="Feature Engineering Categoricals - Train"
+            )
+            ,node(
+                func=process_numeric
+                ,inputs=["train_dropped","parameters"]
+                ,outputs="train_numeric"
+                ,name="Feature Engineering Numericals - Train"
+            )
+            ,node(
+                func=drop_covariates
+                ,inputs=["test","parameters"]
+                ,outputs="test_dropped"
+                ,name="Drop Irrelevant Covariates - Test"
+            )
+            ,node(
+                func=process_categorical
+                ,inputs=["test_dropped","parameters"]
+                ,outputs="test_categorical"
+                ,name="Feature Engineering Categoricals - Test"
+            )
+            ,node(
+                func=process_numeric
+                ,inputs=["test_dropped","parameters"]
+                ,outputs="test_numeric"
+                ,name="Feature Engineering Numericals - Test"
+            )
+            ,node(
+                func=join_num_cat
+                ,inputs=["train_numeric","train_categorical"]
+                ,outputs="train_joined_num_cat"
+                ,name="Join Categorical and Numerical - Train"
+            )
+            ,node(
+                func=join_num_cat
+                ,inputs=["test_numeric","test_categorical"]
+                ,outputs="test_joined_num_cat"
+                ,name="Join Categorical and Numerical - Test"
             )
         ]
     )
